76e5d0e87c1c389fb2a788c6586b986540b2c6d7
Alexander Gotsulenko <axl.harvelle.blake@gmail.com>
2020-12-08 01:46:46 +0200

    lsp-java-download-url to old

--- init.el
+++ init.el
@@ -402,8 +402,47 @@
   :config
   (pyvenv-mode 1))
 
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-(use-package dap-java :ensure nil)
+(use-package lsp-java
+  :init
+  (defun jmi/java-mode-config ()
+    (setq-local tab-width 4
+                c-basic-offset 4)
+    (toggle-truncate-lines 1)
+    (setq-local tab-width 4)
+    (setq-local c-basic-offset 4)
+    (setq lsp-java-jdt-download-url "https://download.eclipse.org/jdtls/milestones/0.57.0/jdt-language-server-0.57.0-202006172108.tar.gz")
+    (lsp))
+
+  :config
+  ;; Enable dap-java
+  (require 'dap-java)
+
+  ;; Support Lombok in our projects, among other things
+  (setq lsp-java-vmargs
+        (list "-noverify"
+              "-Xmx2G"
+              "-XX:+UseG1GC"
+              "-XX:+UseStringDeduplication"
+              (concat "-javaagent:" jmi/lombok-jar)
+              (concat "-Xbootclasspath/a:" jmi/lombok-jar))
+        lsp-file-watch-ignored
+        '(".idea" ".ensime_cache" ".eunit" "node_modules"
+          ".git" ".hg" ".fslckout" "_FOSSIL_"
+          ".bzr" "_darcs" ".tox" ".svn" ".stack-work"
+          "build")
+
+        lsp-java-import-order '["" "java" "javax" "#"]
+        ;; Don't organize imports on save
+        lsp-java-save-action-organize-imports nil
+
+        ;; Formatter profile
+        lsp-java-format-settings-url
+        (concat "file://" jmi/java-format-settings-file))
+
+  :hook (java-mode   . jmi/java-mode-config)
+
+  :demand t
+  :after (lsp lsp-mode dap-mode jmi-init-platform-paths))
 
 (use-package company
   :after lsp-mode
