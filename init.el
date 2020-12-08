c474ef8ed9f9338204d46fc7bf506189b94f14fd
Alexander Gotsulenko <axl.harvelle.blake@gmail.com>
2020-12-09 01:45:31 +0200

    move-text

--- init.el
+++ init.el
@@ -563,6 +563,10 @@
 
 (use-package json-mode)
 
+(use-package move-text
+  :init
+  (move-text-default-bindings))
+
 (use-package vterm
   :commands vterm
   :ensure t
@@ -628,16 +632,3 @@
 ;; ;; Clean up lsp blacklist folders
 ;; (setf (lsp-session-folders-blacklist (lsp-session)) nil)
 ;; (lsp--persist-session (lsp-session))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(docker-compose-mode yasnippet-snippets which-key vterm visual-fill-column use-package typescript-mode smex rainbow-delimiters pyvenv python-mode org-bullets multiple-cursors magit lsp-ui lsp-java lsp-ivy json-mode ivy-rich ibuffer-vc ibuffer-projectile highlight-indent-guides helpful eyebrowse evil-nerd-commenter doom-themes doom-modeline dockerfile-mode dired-single dired-open dired-hide-dotfiles counsel-projectile company-box command-log-mode all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
