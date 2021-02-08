a8afa171260d613902e3f54fde6b248d4526ff46
Alexander Gotsulenko <axl.harvelle.blake@gmail.com>
2021-02-08 23:24:20 +0200

    order package system setup

--- init.el
+++ init.el
@@ -10,6 +10,24 @@
 
 (setq gc-cons-threshold 100000000) ;; 100mb of memory
 
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (use-package page-break-lines)
 (use-package all-the-icons)
 
@@ -31,24 +49,6 @@
 (setq dashboard-set-footer nil)
   ;; (if (equal user-full-name "axl")
   ;;     (add-hook 'after-init-hook 'org-agenda-list))
-
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-  ;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
 
 ;; (setq inhibit-startup-message t)
 
