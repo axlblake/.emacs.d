70d9061345a6d34e812c09fce7c97e7bcc9b469b
Alexander Gotsulenko <axl.harvelle.blake@gmail.com>
2021-03-25 13:09:52 +0200

    dired mode listing switches

--- init.el
+++ init.el
@@ -691,7 +691,7 @@ If popup is focused, delete it."
   :ensure nil
   :commands (dired dired-jump)
   :bind (("C-x C-j" . dired-jump))
-  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :custom ((dired-listing-switches "-laGh1v --group-directories-first"))
   :config
   ;; (evil-collection-define-key 'normal 'dired-mode-map
     ;; "h" 'dired-single-up-directory
